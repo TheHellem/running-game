117b5acb1075798d4b4a4d398a6bf39144f73c07
HenrikH <henrik.hellem@live.com>
2023-01-25 09:38:23 +0100

    Extend Obstacle class and remove beer.js

--- js/obstacles.js
+++ js/obstacles.js
@@ -1,30 +1,37 @@
 class Obstacle {
   constructor(image) {
     this.image = image;
-    this.x = width;
+    this.x = WIDTH;
     this.y = (HEIGHT / 6) * 4.5+25;
-    this.heigth = 50;
+    this.height = 50;
     this.width = 50;
     this.xVelocity = 3;
   }
   draw() {
     this.x -= this.xVelocity;
-    image(this.image, this.x, this.y, this.width, this.heigth);
+    image(this.image, this.x, this.y, this.width, this.height);
   }
   collision(playerInfo) {
-    let obstacleX = this.x + this.width / 2;
-    let obstacleY = this.y + this.height / 2;
+    let beerX = this.x + this.width / 2;
+    let beerY = this.y + this.height / 2;
 
     let playerX = playerInfo.x + playerInfo.width / 2;
     let playerY = playerInfo.y + playerInfo.height / 2;
-  
-  if (dist(obstacleX, obstacleY, playerX, playerY) > 50) {
-    return false;
-  } else {
-    return true;
+
+    if (dist(beerX, beerY, playerX, playerY) > 50) {
+      return false;
+    } else {
+      return true;
+    }
   }
-}
   changeSpeed(speedSetting) {
     this.xVelocity=speedSetting;
+  }
+}
+
+class Beer extends Obstacle {
+  constructor(image){
+    super(image)
+
   }
 }
